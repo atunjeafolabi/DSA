d3c8ec7b3066186d79f8b9f8d33263d5a32aa46c
Atunje <atunje_afolabi@yahoo.com>
2021-07-16 06:17:12 +0100

    Find the maximum value of an array

--- src/com/dsa/ArrayMax.java
+++ src/com/dsa/ArrayMax.java
@@ -12,10 +12,10 @@ public class ArrayMax {
     /** Returns the maximum value of a nonempty array of numbers. */
     private static double arrayMax(double[ ] data) {
         int n = data.length;
-        double currentMax = data[0]; // assume first entry is biggest (for now)
-        for (int j=1; j < n; j++) // consider all other entries
-            if (data[j] > currentMax) // if data[j] is biggest thus far...
-                currentMax = data[j]; // record it as the current max
+        double currentMax = data[0];    // assume first entry is biggest (for now)
+        for (int j=1; j < n; j++)       // consider all other entries
+            if (data[j] > currentMax)   // if data[j] is biggest thus far...
+                currentMax = data[j];   // record it as the current max
 
         return currentMax;
     }
