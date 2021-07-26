1b1453cdf5c936318856ff2d8758779cae97ab85
Atunje <atunje_afolabi@yahoo.com>
2021-07-26 05:13:58 +0100

    Recuresive implementation of factorial function

---
+++ src/com/dsa/Factorial.java
@@ -0,0 +1,22 @@
+package com.dsa;
+
+/**
+ * Code Fragment 5.1
+ * -----------------
+ * A recursive implementation of the factorial function.
+ * I think there is a better and more efficient solution to this
+ */
+public class Factorial {
+    public static void main(String[] args){
+        System.out.println(factorial(5));
+    }
+
+    public static int factorial(int n) throws IllegalArgumentException {
+        if (n < 0)
+            throw new IllegalArgumentException(); // argument must be nonnegative
+        else if (n == 0)
+            return 1; // base case
+        else
+            return n * factorial(n - 1); // recursive case
+    }
+}
