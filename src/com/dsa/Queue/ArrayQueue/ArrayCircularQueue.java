bfebaa1cc7f1553bfb874629c448decc77b8bfd6
Atunje <atunje_afolabi@yahoo.com>
2021-08-10 07:35:36 +0100

    Implementation of circular array queue

---
+++ src/com/dsa/Queue/ArrayQueue/ArrayCircularQueue.java
@@ -0,0 +1,70 @@
+package com.dsa.Queue.ArrayQueue;
+
+/**
+ * Code Fragment 6.10:
+ * -------------------
+ * Array-based implementation of a queue with a circular array that wraps around the end.
+ * Modular arithmetic was used for wrapping around at the end of the array
+ *
+ * @param <E>
+ */
+public class ArrayCircularQueue<E> implements Queue<E> {
+
+    private static final int CAPACITY = 1000;
+    private int front;                                              // index of the front element in the queue
+    private int storedSize;                                         // current number of elements in the queue
+    private E[] data;                                               // array used for storage
+
+    public ArrayCircularQueue() {
+        this(CAPACITY);
+    }
+
+    public ArrayCircularQueue(final int capacity) {
+        data = (E[]) new Object[capacity];
+    }
+
+    @Override
+    public void enqueue(E element) {
+        if (storedSize == data.length) {
+            throw new IllegalStateException("Queue is full!");
+        }
+
+        int avail = (front + storedSize) % data.length;              // modular arithmetic
+        data[avail] = element;
+        storedSize++;
+    }
+
+    /** Removes and returns the first element of the queue (null if empty). */
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        E element = first();
+        data[front] = null;
+        front = (front + 1) % data.length;                           // modular arithmetic
+        storedSize--;
+        return element;
+    }
+
+    @Override
+    public int size() {
+        return storedSize;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return storedSize == 0;
+    }
+
+    /** Returns, but does not remove, the first element of the queue (null if empty). */
+    @Override
+    public E first() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        return data[front];
+    }
+}
