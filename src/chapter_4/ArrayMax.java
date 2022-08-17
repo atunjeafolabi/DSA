76378eb16e5117b9e80f30e7d91d45df2b597985
Atunje <atunje_afolabi@yahoo.com>
2022-08-17 19:08:14 +0200

    created chapter_4 package and moved ArrayMax code into it

--- src/chapter_4/ArrayMax.java
+++ src/chapter_4/ArrayMax.java
@@ -1,4 +1,4 @@
-package com.dsa;
+package chapter_4;
 
 /*
     The algorithm in arrayMax function runs in O(n) time
@@ -12,10 +12,11 @@ public class ArrayMax {
     /** Returns the maximum value of a nonempty array of numbers. */
     private static double arrayMax(double[ ] data) {
         int n = data.length;
-        double currentMax = data[0];    // assume first entry is biggest (for now)
-        for (int j=1; j < n; j++)       // consider all other entries
-            if (data[j] > currentMax)   // if data[j] is biggest thus far...
-                currentMax = data[j];   // record it as the current max
+        double currentMax = data[0];                    // assume first entry is biggest (for now)
+        for (int j=1; j < n; j++) {                     // consider all other entries
+            if (data[j] > currentMax)                   // if data[j] is biggest thus far...
+                currentMax = data[j];                   // record it as the current max
+        }
 
         return currentMax;
     }
