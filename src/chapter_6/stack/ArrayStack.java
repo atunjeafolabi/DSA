dace102b9fd52b53940b948c869008ffa69a06b7
Atunje <atunje_afolabi@yahoo.com>
2022-09-05 06:24:36 +0200

    R6.4 transfer and R6.5 recursive all solution

--- src/chapter_6/stack/ArrayStack.java
+++ src/chapter_6/stack/ArrayStack.java
@@ -65,6 +65,47 @@ public class ArrayStack<E> implements Stack<E> {
         return this.top == -1;
     }
 
+    /*
+     * Problem R-6.4
+     *
+     * Transfers all elements from stack S onto stack T,
+     * so that the element that starts at the top of S is the first to be inserted onto T,
+     * and the element at the bottom of S ends up at the top of T
+     */
+    public static <E> Stack<E> transfer(Stack<E> stackS, Stack<E> stackT) {
+        if (stackS.isEmpty()) {
+            return null;
+        }
+
+        while (stackS.size() > 0) {
+            stackT.push(stackS.pop());
+        }
+
+        return stackT;
+    }
+
+    /*
+     * Problem R-6.5
+     *
+     * Remove all the elements in a stack recursively.
+     */
+    public void recursiveRemoveAll() {
+        if (!isEmpty()) {
+            pop();
+            recursiveRemoveAll();
+        }
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        while (size() != 0) {
+            str = str + " " + pop() + ", ";
+        }
+
+        return str;
+    }
+
     /**
      * Sample usage of the ArrayStack
      */
@@ -78,6 +119,22 @@ public class ArrayStack<E> implements Stack<E> {
     //
     //        System.out.println(integerStack.pop());
     //        System.out.println(integerStack.top());
+
+            // Transfer the elements of stackS onto stackT
+            Stack<String> stackS = new ArrayStack<>();
+            Stack<String> stackT = new ArrayStack<>();
+
+            stackS.push("Red");
+            stackS.push("Green");
+            stackS.push("Blue");
+            stackS.push("Yellow");
+
+            ArrayStack.transfer(stackS, stackT);
+            System.out.println(stackT);
+
+            // Remove all elements recursively
+            ((ArrayStack<String>) stackT).recursiveRemoveAll();
+            System.out.println("Size of stackT after popping all should be zero: " + stackT.size());
 
             // Using Linked Stack
             Stack<Integer> integerStack = new LinkedStack<>();
