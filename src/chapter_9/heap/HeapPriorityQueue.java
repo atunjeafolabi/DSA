d6b0056c8004a1f324fad21f60d9a5f0cde4522b
Atunje <atunje_afolabi@yahoo.com>
2022-12-21 12:50:57 +0100

    added the implementation of HeapPriorityQueue

---
+++ src/chapter_9/heap/HeapPriorityQueue.java
@@ -0,0 +1,134 @@
+package chapter_9.heap;
+
+import chapter_9.priority_queue.AbstractPriorityQueue;
+import chapter_9.priority_queue.Entry;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
+
+    protected ArrayList<Entry<K, V>> heap = new ArrayList<>();
+
+    public HeapPriorityQueue() {
+        super();
+    }
+
+    public HeapPriorityQueue(Comparator<K> comp) {
+        super(comp);
+    }
+
+    // protected utilities
+    protected int parent(int j) {
+        return (j - 1) / 2;
+    }
+
+    protected int left(int j) {
+        return 2 * j + 1;
+    }
+
+    protected int right(int j) {
+        return 2 * j + 2;
+    }
+
+    protected boolean hasLeft(int j) {
+        return this.left(j) < heap.size();
+    }
+
+    protected boolean hasRight(int j) {
+        return this.right(j) < heap.size();
+    }
+
+    /**
+     * Exchanges the entries at indices i and j of the array list.
+     */
+    protected void swap(int i, int j) {
+        Entry<K, V> temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
+
+    /**
+     * Moves the entry at index j higher, if necessary,
+     * to restore the heap property.
+     */
+    protected void upheap(int j) {
+        while (j > 0) {                                           // continue until reaching root (or break statement)
+            int p = parent(j);
+
+            if (compare(heap.get(j), heap.get(p)) >= 0) {
+                break;
+            }
+
+            swap(j, p);
+            j = p;                                               // continue from the parent's location
+        }
+    }
+
+    /**
+     * Moves the entry at index j lower, if necessary,
+     * to restore the heap property.
+     */
+    protected void downheap(int j) {
+
+        while (hasLeft(j)) {                                    // continue to bottom (or break statement)
+            int leftIndex = left(j);
+            int smallChildIndex = leftIndex;                    // although right may be smaller
+
+            if (hasRight(j)) {
+                int rightIndex = right(j);
+                if (compare(heap.get(leftIndex), heap.get(rightIndex)) > 0) {
+                    smallChildIndex = rightIndex;
+                }
+            }
+
+            if (compare(heap.get(smallChildIndex), heap.get(j)) >= 0) {
+                break;
+            }
+
+            swap(j, smallChildIndex);
+            j = smallChildIndex;
+        }
+    }
+
+    @Override
+    public int size() {
+        return heap.size();
+    }
+
+    @Override
+    public Entry<K, V> min() {
+
+        if (heap.isEmpty()) {
+            return null;
+        }
+
+        return heap.get(0);
+    }
+
+    @Override
+    public Entry<K, V> insert(K key, V value) throws IllegalArgumentException {
+        checkKey(key);
+
+        Entry<K, V> newest = new PQEntry<>(key, value);
+        heap.add(newest);
+        upheap(heap.size() - 1);
+
+        return newest;
+    }
+
+    @Override
+    public Entry<K, V> removeMin() {
+
+        if (heap.isEmpty()) {
+            return null;
+        }
+
+        Entry<K, V> temp = heap.get(0);
+        swap(0, heap.size() - 1);
+        heap.remove(heap.size() - 1);
+        downheap(0);
+
+        return temp;
+    }
+}
