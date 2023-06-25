43c6d5d1660f458e5a8a1f3585dd0ebf96605368
Atunje <atunje_afolabi@yahoo.com>
2023-06-25 21:05:52 +0200

    C925: implement a stack using a PriorityQueue

---
+++ src/chapter_9/exercises/C925.java
@@ -0,0 +1,69 @@
+package chapter_9.exercises;
+
+import chapter_6.stack.Stack;
+import chapter_9.heap.HeapAdaptablePriorityQueue;
+
+/**
+ * C-9.25
+ *
+ * Show how to implement the stack ADT using only a priority
+ * queue and one additional integer instance variable.
+ */
+public class C925<E> implements Stack<E> {
+    private HeapAdaptablePriorityQueue queue = new HeapAdaptablePriorityQueue();
+    private int x;
+
+    @Override
+    public int size() {
+        return queue.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    @Override
+    public void push(E e) {
+        /**
+         * Incremental priority key is (-1 * x++)
+         *
+         * We multiplied by -1 to always make the the last inserted
+         * item on the stack be of the highest priority.
+         *
+         * In this case, the lowest (i.e most negative) key
+         * has the highest priority
+         */
+        queue.insert((-1 * x++), e);
+    }
+
+    @Override
+    public E top() {
+        return (E) queue.min().getValue();
+    }
+
+    @Override
+    public E pop() {
+        return (E) queue.removeMin().getValue();
+    }
+
+    public static void main(String[] args) {
+
+        Stack stack = new C925<>();
+
+
+        stack.push("RED");
+        stack.push("ORANGE");
+        stack.push("YELLOW");
+        stack.push("GREEN");
+        stack.push("BLUE");
+
+
+        System.out.println(stack.top());
+        System.out.println(stack.pop());
+        System.out.println(stack.top());
+        System.out.println(stack.pop());
+        System.out.println(stack.top());
+
+    }
+}
